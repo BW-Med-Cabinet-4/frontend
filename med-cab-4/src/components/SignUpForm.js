cafa7eaf28a66ba87142037b69c2f3c200dff1df
Christopher Giroux <cgiroux86@gmail.com>
2020-03-02 15:20:58 -0500

    updated form submit functionality

--- med-cab-4/src/components/SignUpForm.js
+++ med-cab-4/src/components/SignUpForm.js
@@ -7,16 +7,33 @@ const SignUpForm = () => {
         last: '',
         email: ''
     })
+
+    const [team,setTeam] = useState([])
+
     const handleChange = (e) => {
         setUser({...user, [e.target.name] : e.target.value})
         console.log(user)
     }
+
+    const newUser = u => {
+        const member = {
+            first: u.first,
+            last: u.last,
+            email: u.email
+        }
+      setTeam([...team, member])
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault()
+        newUser(user)
+        console.log(team)
+        setUser({first: '', last: '', email:''})
     }
+   
     return (
         <div>
-           <form>
+           <form onSubmit={handleSubmit}>
               <label>First Name:
                 <input onChange={handleChange} type='text' name='first' value={user.first}></input>  
               </label> 
