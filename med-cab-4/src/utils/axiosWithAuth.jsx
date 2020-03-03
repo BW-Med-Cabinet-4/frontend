c26577074b893ab48f2085067c24b9f6a339e5f6
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-03-02 19:51:43 -0500

    added baseURL

--- med-cab-4/src/utils/axiosWithAuth.jsx
+++ med-cab-4/src/utils/axiosWithAuth.jsx
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
     return axios.create({
         // config object
-        baseURL: '',
+        baseURL: 'https://medcabinetbuild.herokuapp.com/api',
         headers: {
             Authorization: token,
         }
